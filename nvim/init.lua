3bce88effe899d70f0cf3a954056cb3676778696
Lime Moose <124237834+lime-moose@users.noreply.github.com>
2024-09-24 12:58:13 +0530

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("configs/options") -- load editor configs
-require("configs/keymaps") -- load keymaps
-require("configs/lazy") -- load lazy plugin manager
--- nvim/lua/configs/options.lua
+++
@@ -1,9 +0,0 @@
---- editor ui-style ---
-vim.opt.number = true -- show line numbers
-vim.opt.laststatus = 0 -- hide status bar
-
---- editor formating style ---
-vim.opt.tabstop = 2 -- change tab space
-vim.opt.shiftwidth = 2 -- do something, idk :)
-vim.opt.softtabstop = 2 -- same space on each tab
-vim.opt.expandtab = true -- control the spaces
