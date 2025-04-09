92d1757fe83c1bef93f1dd46e19d853db8eabd67
Lime Moose <124237834+lime-moose@users.noreply.github.com>
2025-04-09 20:55:30 +0530

    Update tpm-installer.sh

--- tmux/tpm-installer.sh
+++ tmux/tpm-installer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-TPM_HOME="$HOME/.config/tmux/plugins/tpm"
+TPM_HOME="plugins/tpm"
 
 if [ ! -d "$TPM_HOME" ];then
     mkdir -p "$TPM_HOME"
     git clone https://github.com/tmux-plugins/tpm "$TPM_HOME"
 fi
+
+rm "tpm-installer.sh"
