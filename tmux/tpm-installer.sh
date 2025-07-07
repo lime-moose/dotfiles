8b8729d98efa69f7fc1eed2075acd94a1c5f1621
Lime Moose <124237834+lime-moose@users.noreply.github.com>
2025-07-07 19:24:10 +0530

    Delete tmux directory

--- tmux/tpm-installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TPM_HOME="plugins/tpm"
-
-if [ ! -d "$TPM_HOME" ];then
-    mkdir -p "$TPM_HOME"
-    git clone https://github.com/tmux-plugins/tpm "$TPM_HOME"
-fi
-
-rm "tpm-installer.sh"
