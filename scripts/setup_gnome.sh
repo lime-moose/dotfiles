23ed3a72e2433a3ff1672c09e04e65098e5df1c3
Lime Moose <124237834+lime-moose@users.noreply.github.com>
2025-09-06 19:22:44 +0530

    update setup_gnome.sh

--- scripts/setup_gnome.sh
+++ scripts/setup_gnome.sh
@@ -5,3 +5,6 @@ setup-dekstop gnome
 
 # clean the clutters
 apk del epiphany gnome-calendar gnome-software gnome-text-editor gnome-contacts gnome-weather gnome-clocks gnome-maps gnome-music simple-scan totem snapshot decibels gnome-connections baobab gnome-disk-utility gnome-console gnome-shell-extensions gnome-extensions-app gnome-tour firefox yelp gnome-backgrounds
+
+# install adw-theme for gtk3
+apk add adw-gtk3
